8545f4f2881eb49a33b0d55ebb60337c3147d5bc
Emily Nichols <emnnichols@outlook.com>
2024-06-02 19:14:09 -0700

    Moved register/login to Welcome component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +6,5 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-
-  // Function to open dialog when sign up button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '380px'
-    });
-  }
-
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '380px'
-    });
-  }
+  title = 'ghibliFlix';
 }
